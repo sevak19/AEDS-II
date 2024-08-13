520018d495d1f5bdbad4598950ddca628cd07fe4
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-08-13 08:39:15 -0300

    upload palindromo LAB02

---
+++ LAB02/Palindromo.java
@@ -0,0 +1,18 @@
+public class Palindromo {
+    public static void main(String[] args) {
+        
+        String palavra;
+        palavra = MyIO.readLine();
+        int tamanho = palavra.length();
+        if(palavra!="FIM"){
+            do{
+                for(int i = 0;i < tamanho;i++){
+                    for(int j = 0; i < tamanho; i++){
+                        palavra[i] 
+                    }
+                }
+                palavra = MyIO.readLine();
+            }while(palavra != "FIM");
+        }
+    }
+}
