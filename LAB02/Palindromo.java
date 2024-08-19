e82ac61c6e7361bf12d6c7ac9822e4c9bf7c1ab1
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-08-19 17:32:54 -0300

    correcao

--- LAB02/Palindromo.java
+++ LAB02/Palindromo.java
@@ -2,25 +2,27 @@ public class Palindromo {
     public static void main(String[] args) {
         
         String palavra;
-        int contador;
+        
         palavra = MyIO.readLine();
-        int tamanho;
-        if(!palavra.equals("FIM")){
-            do{
-                contador = 0;
-                tamanho = palavra.length();
-                for(int i = 0;i < tamanho;i++){ 
-                    if(palavra.charAt(i) == palavra.charAt(tamanho - 1 - i)){
-                        contador++;
-                    }
-                }
-                if(contador == tamanho){
-                    MyIO.print("SIM\n");
-                }else{
-                    MyIO.print("NAO\n");
-                }
-                palavra = MyIO.readLine();
-            }while(!palavra.equals("FIM"));
+        
+        while(!palavra.equals("FIM")){
+            verificarPalindromo(palavra);
+            palavra = MyIO.readLine();
+        }
+    }
+
+    public static void verificarPalindromo (String palavra){
+        int contador = 0;
+        int tamanho = palavra.length();
+        for(int i = 0;i < tamanho;i++){ 
+            if(palavra.charAt(i) == palavra.charAt(tamanho - 1 - i)){
+                contador++;
+            }
+        }
+        if(contador == tamanho){
+            MyIO.print("SIM\n");
+        }else{
+            MyIO.print("NAO\n");
         }
     }
 }
