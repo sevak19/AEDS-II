0bed685e61ee90b3dc7a86312e2cbc20627ec1da
sevak19 <tu1227csn@gmail.com>
2024-08-22 01:18:46 -0300

    update organizacao && verde lab02

--- LAB02/JAVA LAB02Q02 - Sequencia Espelho/Main.java
+++ LAB02/JAVA LAB02Q02 - Sequencia Espelho/Main.java
@@ -6,24 +6,28 @@ public class Main {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         
-        int num1, num2;
+        int num1, num2, num3;
         List<Character> numeros = new ArrayList<>();
         List<Character> numerosEsp = new ArrayList<>();
-
+        
         num1 = sc.nextInt();
         num2 = sc.nextInt();
         
         for(int i = 0;i <= Math.abs(num1-num2); i++){
-            char ch = (char) (num1 + i);
-            numeros.add(ch);
-            System.out.printf("%c", ch);
+            num3 = (num1 + i);
+            String numStr = Integer.toString(num3);
+            for (char ch : numStr.toCharArray()) {
+                numeros.add(ch);
+                System.out.printf("%c", ch);
+            }
         }
-        for (int i = numeros.size() - 1; i >= 0; i--)
-            numerosEsp.add(numeros.get(i));
         
-        for (int num : numerosEsp)
+        for(int i = numeros.size() - 1; i >= 0; i--)
+            numerosEsp.add(numeros.get(i));
+            
+        for(char num : numerosEsp)
             System.out.printf("%c", num);
-
+            
         sc.close();
     }
 }
