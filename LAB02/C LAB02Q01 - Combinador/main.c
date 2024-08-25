04a02be016637ac9d1d628f7e8a39c83dd65b991
sevak19 <tu1227csn@gmail.com>
2024-08-25 16:28:09 -0300

    update correcao

--- LAB02/C LAB02Q01 - Combinador/main.c
+++ LAB02/C LAB02Q01 - Combinador/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-//LAB02Q01 - Combinador
+// LAB02Q01 - Combinador
 
-int main()
-{
+int main() {
+    char entrada1[100], entrada2[100];
+    int tamanho1, tamanho2;
     
-    char entrada[100];
-    int contador, tamanho;
-
+    while (scanf("%s %s", entrada1, entrada2) != EOF) {
+        char saida[200];
+        int j = 0;
+        
+        tamanho1 = strlen(entrada1);
+        tamanho2 = strlen(entrada2);
+        
+        for(int i = 0; i < tamanho1 && i < tamanho2; i++){
+            saida[j++] = entrada1[i];
+            saida[j++] = entrada2[i];
+        }
+        if(tamanho1 > tamanho2){
+            for(int i = tamanho2; i < tamanho1; i++){
+                saida[j++] = entrada1[i];
+            }
+        }else if(tamanho2 > tamanho1){
+            for(int i = tamanho1; i < tamanho2; i++){
+                saida[j++] = entrada2[i];
+            }
+        }
+        
+        saida[j] = '\0';
+        printf("%s\n", saida);
+    }
     return 0;
 }
--- LAB02/JAVA LAB02Q02 - Sequencia Espelho/Main.java
+++ LAB02/JAVA LAB02Q02 - Sequencia Espelho/Main.java
@@ -1,33 +1,36 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         
         int num1, num2, num3;
-        List<Character> numeros = new ArrayList<>();
-        List<Character> numerosEsp = new ArrayList<>();
         
-        num1 = sc.nextInt();
-        num2 = sc.nextInt();
-        
-        for(int i = 0;i <= Math.abs(num1-num2); i++){
-            num3 = (num1 + i);
-            String numStr = Integer.toString(num3);
-            for (char ch : numStr.toCharArray()) {
-                numeros.add(ch);
-                System.out.printf("%c", ch);
+        while (sc.hasNextInt()){
+            
+            num1 = sc.nextInt();
+            num2 = sc.nextInt();
+            
+            List<Character> numeros = new ArrayList<>();
+            List<Character> numerosEsp = new ArrayList<>();
+            
+            for(int i = 0;i <= Math.abs(num1-num2); i++){
+                num3 = (num1 + i);
+                String numStr = Integer.toString(num3);
+                for (char ch : numStr.toCharArray()) {
+                    numeros.add(ch);
+                    System.out.printf("%c", ch);
+                }
             }
+            
+            for(int i = numeros.size() - 1; i >= 0; i--)
+                numerosEsp.add(numeros.get(i));
+            
+            for(char num : numerosEsp)
+                System.out.printf("%c", num);
+            
+            System.out.println();
         }
-        
-        for(int i = numeros.size() - 1; i >= 0; i--)
-            numerosEsp.add(numeros.get(i));
-            
-        for(char num : numerosEsp)
-            System.out.printf("%c", num);
-            
         sc.close();
     }
 }
