1abd066109f2f4804bffe9d93862100b7b25f90d
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-08-26 16:12:42 -0300

    Upload correcao

--- tps/TP 01/Q03 - Ciframento de César/Main.java
+++ tps/TP 01/Q03 - Ciframento de César/Main.java
@@ -1,5 +1,34 @@
 import java.util.Scanner;
 
-public class Ciframento {
-    
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String entrada = sc.nextLine();
+		
+		while(!entrada.equals("FIM")) {
+        	String saida = Ciframento(entrada);
+        	
+        	System.out.println(saida);
+        	
+        	entrada = sc.nextLine();
+		}
+		sc.close();
+	}
+	
+	public static String Ciframento(String entrada){
+	    int tamanho = entrada.length();
+	    char[] saida = new char[tamanho];
+	    for(int i = 0; i < tamanho; i++){
+	        char c = entrada.charAt(i);
+	        if(c >= 0 && c <= 127){
+	            saida[i] = (char)(c + 3);
+	        }else{
+	            saida[i] = c;
+	        }
+	    }
+	    
+	    return new String(saida);
+	}
 }
