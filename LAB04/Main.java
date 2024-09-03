1a473ee9f0a20c2bb7d115db2c5010f4ca1cd8ab
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-09-03 17:29:28 -0300

    update LAB04Q01 - Sort sort sort

--- LAB04/Main.java
+++ LAB04/Main.java
@@ -9,33 +9,41 @@ public class Main {
         int valor;
         int modulo;
 
-        Numeros[] entrada = new Numeros[N];
-
-        for (int i = 0; i < N; i++) {
-            entrada[i] = new Numeros();
-        }
-
-        for (int i = 0; i < N; i++) {
-            valor = sc.nextInt();
-            modulo = valor % M;
-            entrada[i].setNumero(valor);
-            entrada[i].setModulo(modulo);
-        }
-
-        for (int i = 1; i < N; i++) {
-            Numeros chave = entrada[i];
-            int j = i - 1;
-
-            while (j >= 0 && deveMover(entrada[j], chave)) {
-                entrada[j + 1] = entrada[j];
-                j--;
+        while(M != 0 && N != 00){
+            Numeros[] entrada = new Numeros[N];
+    
+            for (int i = 0; i < N; i++) {
+                entrada[i] = new Numeros();
             }
-            entrada[j + 1] = chave;
+    
+            for (int i = 0; i < N; i++) {
+                valor = sc.nextInt();
+                modulo = valor % M;
+                entrada[i].setNumero(valor);
+                entrada[i].setModulo(modulo);
+            }
+    
+            for (int i = 1; i < N; i++) {
+                Numeros chave = entrada[i];
+                int j = i - 1;
+    
+                while (j >= 0 && deveMover(entrada[j], chave)) {
+                    entrada[j + 1] = entrada[j];
+                    j--;
+                }
+                entrada[j + 1] = chave;
+            }
+            
+            System.out.println(N + " " + M);
+    
+            for(int i = 0; i < N; i++) {
+                System.out.println(entrada[i].getNumero());
+            }
+            N = sc.nextInt();
+            M = sc.nextInt();
         }
-        
-        for(int i = 0; i < N; i++) {
-            System.out.println(entrada[i].getNumero());
-        }
+
+        System.out.println(N + " " + M);
 
         sc.close();
     }
@@ -45,9 +53,17 @@ public class Main {
             return true;
         }else if (anterior.getModulo() == chave.getModulo()) {
             if (anterior.getNumero() % 2 != 0 && chave.getNumero() % 2 != 0) {
-                return anterior.getNumero() < chave.getNumero();
+                if(anterior.getNumero() < chave.getNumero()){
+                    return anterior.getNumero() < chave.getNumero();
+                }
             }else if (anterior.getNumero() % 2 == 0 && chave.getNumero() % 2 == 0) {
-                return anterior.getNumero() > chave.getNumero();
+                if(anterior.getNumero() > chave.getNumero()){
+                    return anterior.getNumero() > chave.getNumero();
+                }
+            }else if(anterior.getNumero() % 2 != 0 && chave.getNumero() % 2 == 0) {
+                return false;
+            }else if(anterior.getNumero() % 2 == 0 && chave.getNumero() % 2 != 0){
+                return true;
             }
         }
         return false;
