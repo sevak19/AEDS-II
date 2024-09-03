9d5d47c96d356cfe8f82c42acfafa33d29a9097b
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-09-03 16:56:33 -0300

    update LAB04 Q01 - Sort sort sort

--- LAB04/Main.java
+++ LAB04/Main.java
@@ -4,31 +4,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int ancora;
         int N = sc.nextInt();
         int M = sc.nextInt();
+        int valor;
+        int modulo;
 
-        int[] entrada = new int[N];
-        int[] modulos = new int[N];
-        int[] ordenados = new int[N];
+        Numeros[] entrada = new Numeros[N];
 
-        for(int i = 0;i< N; i++){
-            entrada[i] = sc.nextInt();
-            modulos[i] = entrada[i] % M;
+        for (int i = 0; i < N; i++) {
+            entrada[i] = new Numeros();
         }
-        for(int i = 1;i< N; i++){
-            ancora = modulos[i];
-            if(ancora > modulos[i-1]){
-                ancora = modulos[i-1];
-            } else {
-                ordenados[i] = entrada[i];
+
+        for (int i = 0; i < N; i++) {
+            valor = sc.nextInt();
+            modulo = valor % M;
+            entrada[i].setNumero(valor);
+            entrada[i].setModulo(modulo);
+        }
+
+        for (int i = 1; i < N; i++) {
+            Numeros chave = entrada[i];
+            int j = i - 1;
+
+            while (j >= 0 && deveMover(entrada[j], chave)) {
+                entrada[j + 1] = entrada[j];
+                j--;
             }
+            entrada[j + 1] = chave;
         }
-        for(int numero : ordenados){
-            System.out.println(numero);
+        
+        for(int i = 0; i < N; i++) {
+            System.out.println(entrada[i].getNumero());
         }
 
         sc.close();
+    }
+
+    private static boolean deveMover(Numeros anterior, Numeros chave) {
+        if (anterior.getModulo() > chave.getModulo()) {
+            return true;
+        }else if (anterior.getModulo() == chave.getModulo()) {
+            if (anterior.getNumero() % 2 != 0 && chave.getNumero() % 2 != 0) {
+                return anterior.getNumero() < chave.getNumero();
+            }else if (anterior.getNumero() % 2 == 0 && chave.getNumero() % 2 == 0) {
+                return anterior.getNumero() > chave.getNumero();
+            }
+        }
+        return false;
     }
 }
 
