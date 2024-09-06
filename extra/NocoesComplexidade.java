7f1de946124ddba6de05ff7fab30c43a9fb8c38e
sevak19 <tu1227csn@gmail.com>
2024-09-06 20:18:33 -0300

    Upload extra/Nocoes de Complexidade

--- extra/NocoesComplexidade.java
+++ extra/NocoesComplexidade.java
@@ -10,6 +10,30 @@ public class NocoesComplexidade {
         
         for(int i = 0; i < n;i++){
             array[i] = sc.nextInt();
+        }
+
+        int mov = 0;
+
+        for(int i = 0; i < (n-1);i++){
+            int menor = i;
+            for(int j = (i+1);j < n;j++){
+                if(array[menor] > array[j]){
+                    menor = j;
+                }
+            }
+
+            int temp = array[menor];
+            array[menor] = array[i];
+            array[i] = temp;
+
+            mov+=3;
+        }
+
+        System.out.println("Teoria: " + (3*n-3));
+        System.out.println("Prática: " + mov);
+        
+        System.out.println("Array ordenado(selection sort): ");
+        for(int i = 0; i < n;i++){
             System.out.println(array[i]);
         }
 
