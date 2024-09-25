26575f5194123ba2e1c00d308cb49f01c79bcb69
sevak19 <tu1227csn@gmail.com>
2024-09-24 23:52:38 -0300

    Upload TP02Q01

---
+++ TPS/TP 02/TP02Q01 - Classe em Java/Main.java
@@ -0,0 +1,191 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "pokemon.csv";
+        ArrayList<Pokemon> pokemons = new ArrayList<Pokemon>();
+
+        try {
+            Scanner sc = new Scanner(new File(path));
+            sc.nextLine();
+        while (sc.hasNextLine()) {
+            pokemons.add(new Pokemon(sc.nextLine()));
+        }
+        sc.close();
+        } catch (Exception e) { System.out.println("error");}
+
+    }
+}
+
+class Pokemon {
+    private int id;
+    private int generation;
+    private String name;
+    private String description;
+    private ArrayList<String> types;
+    private ArrayList<String> abilities;
+    private double weight;
+    private double height;
+    private int captureRate;
+    private boolean isLegendary;
+    private LocalDate captureDate;
+
+    public Pokemon () { }
+
+    public Pokemon (int id,
+                   int generation,
+                   String name,
+                   String description,
+                   ArrayList<String> types,
+                   ArrayList<String> abilities,
+                   double weight,
+                   double heigth,
+                   int captureRate,
+                   boolean isLegendary,
+                   LocalDate captureDate
+                   ) {
+        setGeneration(generation);
+        setName(name);
+        setDescription(description);
+        setTypes(types);
+        setAbilities(abilities);
+        setWeight(weight);
+        setHeight(height);
+        setCaptureRate(captureRate);
+        setIsLegendary(isLegendary);
+        setCaptureDate(captureDate);
+    }
+
+    public Pokemon (String linha) {
+        String[] item = linha.split(",");
+
+        setId(Integer.parseInt(item[0]));
+
+        setGeneration(Integer.parseInt(item[1]));
+
+        setName(item[2]);
+
+        setDescription(item[3]);
+
+        ArrayList<String> types = new ArrayList<>();
+        types.add(item[4]);
+        if (!item[5].isEmpty())
+            types.add(item[5]);
+        setTypes(types);
+
+        int counter = 6;
+        boolean IsDouble = false;
+        ArrayList<String> abilities = new ArrayList<>();
+        while (!IsDouble) {
+            try {
+                Double.parseDouble(item[counter]); 
+                IsDouble = true;
+            } catch (NumberFormatException e) {
+                if(!item[counter].isEmpty()){
+                    String abilitie = item[counter].replaceAll("[\\[\\]'\"']", "").trim();
+                    abilities.add(abilitie);
+                    counter++;
+                }else{
+                    IsDouble = true;
+                }
+            }
+        }
+        setAbilities(abilities);
+
+        if(!item[counter].isEmpty())
+            setWeight(Double.parseDouble(item[counter]));
+        counter++;
+
+        if(!item[counter].isEmpty())
+            setHeight(Double.parseDouble(item[counter]));
+        counter++;
+
+        setCaptureRate(Integer.parseInt(item[counter]));
+        counter++;
+
+        setIsLegendary(item[counter].equals("1"));
+        counter++;
+
+        System.out.println(item[counter]);
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+    public int getId () {
+        return id;
+    }
+
+    public void setGeneration (int generation) {
+        this.generation = generation;
+    }
+    public int getGeneration () {
+        return generation;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+    public String getName () {
+        return name;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+    public String getDescription () {
+        return description;
+    }
+
+    public void setTypes (ArrayList<String> types) {
+        this.types = types;
+    }
+    public ArrayList<String> getTypes () {
+        return types;
+    }
+
+    public void setAbilities (ArrayList<String> abilities) {
+        this.abilities = abilities;
+    }
+    public ArrayList<String> getAbilities () {
+        return abilities;
+    }
+
+    public void setWeight (double weight) {
+        this.weight = weight;
+    }
+    public double getWeight () {
+        return weight;
+    }
+
+    public void setHeight (double height) {
+        this.height = height;
+    }
+    public double getHeight () {
+        return height;
+    }
+
+    public void setCaptureRate (int captureRate) {
+        this.captureRate = captureRate;
+    }
+    public double getCaptureRate () {
+        return captureRate;
+    }
+
+    public void setIsLegendary (boolean isLegendary) {
+        this.isLegendary = isLegendary;
+    }
+    public boolean getIsLegendary () {
+        return isLegendary;
+    }
+
+    public void setCaptureDate(LocalDate captureDate) {
+        this.captureDate = captureDate;
+    }
+    public LocalDate getCaptureDate() {
+        return captureDate;
+    }
+}
