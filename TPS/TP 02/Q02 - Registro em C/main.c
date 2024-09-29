61e7e9275e94f3aea4bda4e151a7d7ab1f8c862f
sevak19 <tu1227csn@gmail.com>
2024-09-28 23:46:48 -0300

    Update Q02 - C

--- TPS/TP 02/Q02 - Registro em C/main.c
+++ TPS/TP 02/Q02 - Registro em C/main.c
@@ -5,6 +5,8 @@
 #include <stdbool.h>
 #include <time.h>
 
+#define NUM_POKEMONS 801
+
 typedef struct {
     int ano;
     int mes;
@@ -25,26 +27,80 @@ typedef struct {
     LocalDate captureDate;
 } Pokemon;
 
-int main () {
-    Pokemon pokedex[801];
+void ler(Pokemon pokedex[NUM_POKEMONS]);
+void imprimir(Pokemon* pokemon);
+Pokemon clone(Pokemon* pokemon);
 
-    FILE *file = fopen("pokemon.csv", "r");
+int getId(Pokemon* pokemon);
+void setId(Pokemon* pokemon, int id);
+
+int getGeneration(Pokemon* pokemon);
+void setGeneration(Pokemon* pokemon, int generation);
+
+char* getName(Pokemon* pokemon);
+void setName(Pokemon* pokemon, char nome[]);
+
+char* getDescription(Pokemon* pokemon);
+void setDescription(Pokemon* pokemon, char descricao[]);
+
+char* getType(Pokemon* pokemon, int i);
+void setType(Pokemon* pokemon, int i, char tipo[]);
+
+char* getAbility(Pokemon* pokemon, int i);
+void setAbility(Pokemon* pokemon, int i, char habilidade[]);
+
+double getWeight(Pokemon* pokemon);
+void setWeight(Pokemon* pokemon, double weight);
+
+double getHeight(Pokemon* pokemon);
+void setHeight(Pokemon* pokemon, double height);
+
+int getCaptureRate(Pokemon* pokemon);
+void setCaptureRate(Pokemon* pokemon, int captureRate);
+
+bool isLegendary(Pokemon* pokemon);
+void setLegendary(Pokemon* pokemon, bool isLegendary);
+
+LocalDate getCaptureDate(Pokemon* pokemon);
+void setCaptureDate(Pokemon* pokemon, LocalDate captureDate);
+
+int main () {
+    Pokemon pokedex[NUM_POKEMONS];
+
+    ler(pokedex);
+
+    //Pokemon clonado = clone(&pokedex[787]);
+    //imprimir(&clonado);
+
+    char line[3];
+    scanf(" %[^\n]", line);
+    while(strcmp(line, "FIM") != 0) {
+        int num = strtol(line, NULL, 10);
+        imprimir(&pokedex[num-1]);
+        scanf(" %[^\n]", line);
+    }
+
+    return 0;
+}
+
+void ler(Pokemon pokedex[NUM_POKEMONS]) {
+    FILE *file = fopen("/tmp/pokemon.csv", "r");
 
     if (file == NULL) {
         printf("Erro ao abrir o arquivo.\n");
-        return 1;
+        exit(1);
     }
 
     char line[256];
     fgets(line, sizeof(line), file);
     int counter = 0;
     while (fgets(line, sizeof(line), file) != NULL) {
-        Pokemon pokemon;
-        int id, generation;
+        int id, generation, captureRate, dia, mes, ano;
         char name[50] = "NULL", description[50] = "NULL";
         char types[2][50], abilities[10][50];
         char auxiliar[10];
         double weight, height;
+        bool isLegendary;
         strcpy(auxiliar, "NULL");
         for (int i = 0; i < 2; i++) {
             strcpy(types[i], "NULL");
@@ -83,21 +139,15 @@ int main () {
             int j = 0;
             aux++;
             while(line[aux] != ']') {
-                // Pula aspas duplas, colchetes e aspas simples
                 while(line[aux] == '"' || line[aux] == '[' || line[aux] == '\'') {
                     aux++;
                 }
-
                 i = 0;
-
-                // Copia a habilidade até encontrar a próxima aspa simples
                 while(line[aux] != '\'') {
                     abilities[j][i] = line[aux];
                     i++, aux++;
                 }
-
                 abilities[j][i] = '\0';
-
                 j++, aux++;
                 while (line[aux] == ',' || line[aux] == ' ' || line[aux] == '\'') {
                     aux++;
@@ -120,6 +170,45 @@ int main () {
             }
             auxiliar[i] = '\0';
             height = strtod(auxiliar, NULL);
+            aux ++;
+            i = 0;
+            strcpy(auxiliar, "NULL");
+            while(line[aux] != ',') {
+                auxiliar[i] = line[aux];
+                i++, aux++;
+            }
+            auxiliar[i] = '\0';
+            captureRate = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            if(line[aux] == '1') {
+                isLegendary = true;
+            } else {
+                isLegendary = false;
+            }
+            aux += 2;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 2; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            dia = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 2; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            mes = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 4; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            ano = (int) strtol(auxiliar, NULL, 10);
         }else if(counter < 99) {
             id = (line[0] - '0') * 10 + (line[1] - '0');
             generation = line[3] - '0';
@@ -151,21 +240,15 @@ int main () {
             int j = 0;
             aux++;
             while(line[aux] != ']'){
-                // Pula aspas duplas, colchetes e aspas simples
                 while(line[aux] == '"' || line[aux] == '[' || line[aux] == '\'') {
                     aux++;
                 }
-
                 i = 0;
-
-                // Copia a habilidade até encontrar a próxima aspa simples
                 while(line[aux] != '\'') {
                     abilities[j][i] = line[aux];
                     i++, aux++;
                 }
-
                 abilities[j][i] = '\0';
-
                 j++, aux++;
                 while (line[aux] == ',' || line[aux] == ' ' || line[aux] == '\'') {
                     aux++;
@@ -188,6 +271,45 @@ int main () {
             }
             auxiliar[i] = '\0';
             height = strtod(auxiliar, NULL);
+            aux ++;
+            i = 0;
+            strcpy(auxiliar, "NULL");
+            while(line[aux] != ',') {
+                auxiliar[i] = line[aux];
+                i++, aux++;
+            }
+            auxiliar[i] = '\0';
+            captureRate = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            if(line[aux] == '1') {
+                isLegendary = true;
+            } else {
+                isLegendary = false;
+            }
+            aux += 2;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 2; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            dia = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 2; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            mes = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 4; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            ano = (int) strtol(auxiliar, NULL, 10);
         }else {
             id = (line[0] - '0') * 100 + (line[1] - '0') * 10 + (line[2] - '0');
             generation = line[4] - '0';
@@ -219,22 +341,15 @@ int main () {
             int j = 0;
             aux++;
             while(line[aux] != ']') {
-                // Pula aspas duplas, colchetes e aspas simples
                 while(line[aux] == '"' || line[aux] == '[' || line[aux] == '\'') {
                     aux++;
                 }
-
                 i = 0;
-
-                // Copia a habilidade até encontrar a próxima aspa simples
                 while(line[aux] != '\'') {
                     abilities[j][i] = line[aux];
                     i++, aux++;
                 }
-
                 abilities[j][i] = '\0';
-                
-
                 j++, aux++;
                 while (line[aux] == ',' || line[aux] == ' ' || line[aux] == '\'') {
                     aux++;
@@ -257,56 +372,207 @@ int main () {
             }
             auxiliar[i] = '\0';
             height = strtod(auxiliar, NULL);
+            aux ++;
+            i = 0;
+            strcpy(auxiliar, "NULL");
+            while(line[aux] != ',') {
+                auxiliar[i] = line[aux];
+                i++, aux++;
+            }
+            auxiliar[i] = '\0';
+            captureRate = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            if(line[aux] == '1') {
+                isLegendary = true;
+            } else {
+                isLegendary = false;
+            }
+            aux += 2;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 2; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            dia = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 2; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            mes = (int) strtol(auxiliar, NULL, 10);
+            aux ++;
+            strcpy(auxiliar, "NULL");
+            for( i = 0; i < 4; i++ ) {
+                auxiliar[i] = line[aux];
+                aux++;
+            }
+            auxiliar[i+1] = '\0';
+            ano = (int) strtol(auxiliar, NULL, 10);
         }
-        pokedex[counter] = pokemon;
-        pokedex[counter].id = id;
-        pokedex[counter].generation = generation;
-        strcpy(pokedex[counter].name, name);
-        strcpy(pokedex[counter].description, description);
-        strcpy(pokedex[counter].types[0], types[0]);
-        strcpy(pokedex[counter].types[1], types[1]);
-        pokedex[counter].weight = weight;
-        pokedex[counter].height = height;
-        //printf("id = %d, gen = %d, name = %s, description = %s, types = %s, %s, abilities = ", pokedex[counter].id, pokedex[counter].generation, pokedex[counter].name, pokedex[counter].description, pokedex[counter].types[0], pokedex[counter].types[1]);
+        setId(&pokedex[counter], id);
+        setGeneration(&pokedex[counter], generation);
+        setName(&pokedex[counter], name);
+        setDescription(&pokedex[counter], description);
+        setType(&pokedex[counter], 0, types[0]);
+        setType(&pokedex[counter], 1, types[1]);
         int i =0;
         while(strcmp(abilities[i], "NULL") != 0) {
-            strcpy(pokedex[counter].abilities[i], abilities[i]);
-            //printf("%s, ", pokedex[counter].abilities[i]);
+            setAbility(&pokedex[counter], i, abilities[i]);
             i++;
         }
+        setAbility(&pokedex[counter], i, "");
+        setWeight(&pokedex[counter], weight);
+        setHeight(&pokedex[counter], height);
+        setCaptureRate(&pokedex[counter], captureRate);
+        setLegendary(&pokedex[counter], isLegendary);
+        LocalDate captureDate;
+        captureDate.dia = dia;
+        captureDate.mes = mes;
+        captureDate.ano = ano;
+        setCaptureDate(&pokedex[counter], captureDate);
 
         counter++;
     }
 
     int fclose(FILE *file);
+}
 
-    if (file == NULL) {
-        printf("fechado com sucesso.\n");
-        return 1;
+void imprimir(Pokemon* pokemon) {
+    printf("[#%d -> ", getId(pokemon));
+    printf("%s: ", getName(pokemon));
+    printf("%s - ", getDescription(pokemon));
+    if (strlen(getType(pokemon, 1)) > 0) {
+        printf("['%s', '%s'] - [", getType(pokemon, 0), getType(pokemon, 1));
+    } else {
+        printf("['%s'] - [", getType(pokemon, 0));
     }
-    // Criando um Pokémon
-    Pokemon pikachu;
+    for (int i = 0; i < 10 && strlen(getAbility(pokemon, i)) > 0; i++) {
+        printf("'%s'", getAbility(pokemon, i));
+        if (i < 9 && strlen(getAbility(pokemon, i + 1)) > 0) {
+            printf(", ");
+        }
+    }
+    printf("] - %.1fkg - ", getWeight(pokemon));
+    printf("%.1fm - ", getHeight(pokemon));
+    printf("%d%% - ", getCaptureRate(pokemon));
+    printf("%s - ", isLegendary(pokemon) ? "true" : "false");
+    printf("%d gen] - ", getGeneration(pokemon));
+    LocalDate captureDate = getCaptureDate(pokemon);
+    printf("%02d/%02d/%d\n", captureDate.dia, captureDate.mes, captureDate.ano);
+}
 
-    // Atribuindo valores ao Pokémon
-    pikachu.id = 25;
-    pikachu.generation = 1;
-    snprintf(pikachu.name, 50, "Pikachu");
-    snprintf(pikachu.description, 50, "Electric type mouse Pokémon");
-    pikachu.weight = 6.0;
-    pikachu.height = 0.4;
-    pikachu.captureRate = 190;
-    pikachu.isLegendary = false;
+Pokemon clone(Pokemon* pokemon) {
+    Pokemon newPokemon;
 
-    // Atribuindo uma data de captura
-    pikachu.captureDate.ano = 2024;
-    pikachu.captureDate.mes = 9;
-    pikachu.captureDate.dia = 26;
+    setId(&newPokemon, getId(pokemon));
+    setGeneration(&newPokemon, getGeneration(pokemon));
+    setName(&newPokemon, getName(pokemon));
+    setDescription(&newPokemon, getDescription(pokemon));
+    for (int i = 0; i < 2; i++) {
+        setType(&newPokemon, i, getType(pokemon, i));
+    }
+    for (int i = 0; i < 10; i++) {
+        setAbility(&newPokemon, i, getAbility(pokemon, i));
+    }
+    setWeight(&newPokemon, getWeight(pokemon));
+    setHeight(&newPokemon, getHeight(pokemon));
+    setCaptureRate(&newPokemon, getCaptureRate(pokemon));
+    setLegendary(&newPokemon, isLegendary(pokemon));
+    setCaptureDate(&newPokemon, getCaptureDate(pokemon));
 
-    // Exibindo a data de captura
-    printf("Capture Date: %d-%02d-%02d\n", 
-           pikachu.captureDate.ano, 
-           pikachu.captureDate.mes, 
-           pikachu.captureDate.dia);
+    return newPokemon;
+}
 
-    return 0;
+int getId(Pokemon* pokemon) {
+    return pokemon->id;
+}
+void setId(Pokemon* pokemon, int id) {
+    pokemon->id = id;
+}
+
+
+int getGeneration(Pokemon* pokemon) {
+    return pokemon->generation;
+}
+void setGeneration(Pokemon* pokemon, int generation) {
+    pokemon->generation = generation;
+}
+
+
+char* getName(Pokemon* pokemon) {
+    return pokemon->name;
+}
+void setName(Pokemon* pokemon, char nome[]) {
+    strncpy(pokemon->name, nome, sizeof(pokemon->name) - 1);
+    pokemon->name[sizeof(pokemon->name) - 1] = '\0';
+}
+
+
+char* getDescription(Pokemon* pokemon) {
+    return pokemon->description;
+}
+void setDescription(Pokemon* pokemon, char descricao[]) {
+    strncpy(pokemon->description, descricao, sizeof(pokemon->description) - 1);
+    pokemon->description[sizeof(pokemon->description) - 1] = '\0';
+}
+
+
+char* getType(Pokemon* pokemon, int i) {
+    return pokemon->types[i];
+}
+void setType(Pokemon* pokemon, int i, char tipo[]) {
+    strncpy(pokemon->types[i], tipo, sizeof(pokemon->types[i]) - 1);
+    pokemon->types[i][sizeof(pokemon->types[i]) - 1] = '\0';
+}
+
+
+char* getAbility(Pokemon* pokemon, int i) {
+    return pokemon->abilities[i];
+}
+void setAbility(Pokemon* pokemon, int i, char habilidade[]) {
+    strncpy(pokemon->abilities[i], habilidade, sizeof(pokemon->abilities[i]) - 1);
+    pokemon->abilities[i][sizeof(pokemon->abilities[i]) - 1] = '\0';
+}
+
+
+double getWeight(Pokemon* pokemon) {
+    return pokemon->weight;
+}
+void setWeight(Pokemon* pokemon, double weight) {
+    pokemon->weight = weight;
+}
+
+
+double getHeight(Pokemon* pokemon) {
+    return pokemon->height;
+}
+void setHeight(Pokemon* pokemon, double height) {
+    pokemon->height = height;
+}
+
+
+int getCaptureRate(Pokemon* pokemon) {
+    return pokemon->captureRate;
+}
+void setCaptureRate(Pokemon* pokemon, int captureRate) {
+    pokemon->captureRate = captureRate;
+}
+
+
+bool isLegendary(Pokemon* pokemon) {
+    return pokemon->isLegendary;
+}
+void setLegendary(Pokemon* pokemon, bool isLegendary) {
+    pokemon->isLegendary = isLegendary;
+}
+
+
+LocalDate getCaptureDate(Pokemon* pokemon) {
+    return pokemon->captureDate;
+}
+void setCaptureDate(Pokemon* pokemon, LocalDate captureDate) {
+    pokemon->captureDate = captureDate;
 }
