b2373d9d0fb58bc3ace5d14820dbca86d4b96bc9
sevak19 <tu1227csn@gmail.com>
2024-10-10 14:56:18 -0300

    Upload Q18

---
+++ TPS/TP 02/Q18 - Quicksort PARCIAL/Main.java
@@ -0,0 +1,327 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+
+public class Main {
+    static int comp = 0;
+    static int mov = 0;
+    public static void main(String[] args) {
+        String path = "/tmp/pokemon.csv";
+
+        PokemonReader reader = new PokemonReader(path);
+        ArrayList<Pokemon> pokemons = reader.readPokemons();
+
+        ArrayList<Pokemon> entrada = new ArrayList<Pokemon>();
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!input.equals("FIM")) {
+            int number = Integer.parseInt(input);
+
+            entrada.add(pokemons.get(number - 1));
+
+            input = sc.nextLine();
+        }
+
+        long start = System.nanoTime();
+
+        Sort.sort(entrada);
+
+        long end = System.nanoTime();
+        double executionTime = (end - start);
+
+        for(int i =0; i < 10; i++) {
+            entrada.get(i).imprimir();
+        }
+
+
+        String conteudo = "1509171" + "\t" + comp + "comp\t" + mov + "mov\t" + executionTime + "ms";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("1509171_QuicksortPARCIAL.txt"))) {
+            writer.write(conteudo);
+        } catch (IOException e) {
+            System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+}
+
+class Sort {
+    private static int k = 10;
+    public static void sort(ArrayList<Pokemon> lista) {
+        quicksort(0, lista.size()-1, lista);
+    }
+
+    private static void quicksort(int esq, int dir, ArrayList<Pokemon> lista) {
+        int i = esq, j = dir;
+        Pokemon pivo = lista.get((dir+esq)/2);
+        while (i <= j) {
+            Main.comp++;
+            while (lista.get(i).getGeneration() < pivo.getGeneration() ||
+                  (lista.get(i).getGeneration() == pivo.getGeneration() && lista.get(i).getName().compareTo(pivo.getName()) < 0)) {
+                i++;
+            }
+            Main.comp++;
+            while (lista.get(j).getGeneration() > pivo.getGeneration() ||
+                  (lista.get(j).getGeneration() == pivo.getGeneration() && lista.get(j).getName().compareTo(pivo.getName()) > 0)) {
+            j--;
+            }
+
+            if (i <= j) {
+                swap(i, j, lista);
+                i++;
+                j--;
+            }
+        }
+        if (esq < j)  quicksort(esq, j, lista);
+        if (i < dir && i < k)  quicksort(i, dir, lista);
+    }
+
+    private static void swap(int A, int B, ArrayList<Pokemon> lista) {
+        Pokemon temp = lista.get(A);
+        lista.set(A, lista.get(B));
+        lista.set(B, temp);
+        Main.mov+=3;
+    }
+}
+
+class Pokemon {
+    private int id;
+    private int generation;
+    private String name;
+    private String description;
+    private ArrayList<String> types;
+    private ArrayList<String> abilities;
+    private double weight;
+    private double height;
+    private int captureRate;
+    private boolean isLegendary;
+    private LocalDate captureDate;
+
+    public Pokemon () { }
+
+    public Pokemon (int id,
+                   int generation,
+                   String name,
+                   String description,
+                   ArrayList<String> types,
+                   ArrayList<String> abilities,
+                   double weight,
+                   double heigth,
+                   int captureRate,
+                   boolean isLegendary,
+                   LocalDate captureDate
+                   ) {
+        setId(id);
+        setGeneration(generation);
+        setName(name);
+        setDescription(description);
+        setTypes(types);
+        setAbilities(abilities);
+        setWeight(weight);
+        setHeight(height);
+        setCaptureRate(captureRate);
+        setIsLegendary(isLegendary);
+        setCaptureDate(captureDate);
+    }
+
+    public Pokemon (String linha) {
+        String[] item = linha.split(",");
+
+        setId(Integer.parseInt(item[0]));
+
+        setGeneration(Integer.parseInt(item[1]));
+
+        setName(item[2]);
+
+        setDescription(item[3]);
+
+        ArrayList<String> types = new ArrayList<>();
+        types.add(item[4]);
+        if (!item[5].isEmpty())
+            types.add(item[5]);
+        setTypes(types);
+
+        int counter = 6;
+        boolean IsDouble = false;
+        ArrayList<String> abilities = new ArrayList<>();
+        while (!IsDouble) {
+            try {
+                Double.parseDouble(item[counter]); 
+                IsDouble = true;
+            } catch (NumberFormatException e) {
+                if(!item[counter].isEmpty()){
+                    String abilitie = item[counter].replaceAll("[\\[\\]'\"']", "").trim();
+                    abilities.add(abilitie);
+                    counter++;
+                }else{
+                    IsDouble = true;
+                }
+            }
+        }
+        setAbilities(abilities);
+
+        if(!item[counter].isEmpty())
+            setWeight(Double.parseDouble(item[counter]));
+        counter++;
+
+        if(!item[counter].isEmpty())
+            setHeight(Double.parseDouble(item[counter]));
+        counter++;
+
+        setCaptureRate(Integer.parseInt(item[counter]));
+        counter++;
+
+        setIsLegendary(item[counter].equals("1"));
+        counter++;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        setCaptureDate(LocalDate.parse(item[counter], formatter));
+    }
+
+    public Pokemon clone() {
+        Pokemon clone = new Pokemon(getId(), getGeneration(), getName(), getDescription(), getTypes(), getAbilities(), getWeight(), getHeight(), getCaptureRate(), getIsLegendary(), getCaptureDate());
+        return clone;
+    }
+ 
+    public void imprimir() {
+        System.out.printf("[#" + id + " -> " + name + ": " + description + " - [");
+        int counter = 1;
+        for(String type : types) {
+            if(counter == 1){
+                System.out.printf("'" + type + "'");
+                counter++;
+            }else{
+                System.out.printf(", '" + type + "'");
+            }
+        }
+        System.out.printf("] - [");
+        counter = 1;
+        for(String abilitie : abilities) {
+            if(counter == 1){
+                System.out.printf("'" + abilitie + "'");
+                counter++;
+            }else{
+                System.out.printf(", '" + abilitie + "'");
+            }
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.printf("] - " + weight + "kg - " + height + "m - " + captureRate + "%% - " + isLegendary + " - " + generation + " gen] - " + captureDate.format(formatter));
+        System.out.println();
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+    public int getId () {
+        return id;
+    }
+
+    public void setGeneration (int generation) {
+        this.generation = generation;
+    }
+    public int getGeneration () {
+        return generation;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+    public String getName () {
+        return name;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+    public String getDescription () {
+        return description;
+    }
+
+    public void setTypes (ArrayList<String> types) {
+        this.types = types;
+    }
+    public ArrayList<String> getTypes () {
+        return types;
+    }
+    public String getType (int index) {
+        return types.get(index);
+    }
+
+    public void setAbilities (ArrayList<String> abilities) {
+        this.abilities = abilities;
+    }
+    public ArrayList<String> getAbilities () {
+        return abilities;
+    }
+
+    public void setWeight (double weight) {
+        this.weight = weight;
+    }
+    public double getWeight () {
+        return weight;
+    }
+
+    public void setHeight (double height) {
+        this.height = height;
+    }
+    public double getHeight () {
+        return height;
+    }
+
+    public void setCaptureRate (int captureRate) {
+        this.captureRate = captureRate;
+    }
+    public int getCaptureRate () {
+        return captureRate;
+    }
+
+    public void setIsLegendary (boolean isLegendary) {
+        this.isLegendary = isLegendary;
+    }
+    public boolean getIsLegendary () {
+        return isLegendary;
+    }
+
+    public void setCaptureDate(LocalDate captureDate) {
+        this.captureDate = captureDate;
+    }
+    public LocalDate getCaptureDate() {
+        return captureDate;
+    }
+}
+
+class PokemonReader {
+
+    private String filePath;
+
+    public PokemonReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public ArrayList<Pokemon> readPokemons() {
+        ArrayList<Pokemon> pokemons = new ArrayList<>();
+        
+        try {
+            Scanner sc = new Scanner(new File(filePath));
+            sc.nextLine();
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (!line.isEmpty()) {
+                    pokemons.add(new Pokemon(line));
+                }
+            }
+            sc.close();
+        }
+
+        catch (FileNotFoundException e) { System.err.println("Arquivo não encontrado: " + filePath); }
+        catch (Exception e) { System.err.println("Erro ao ler o arquivo: " + e.getMessage()); }
+        
+        return pokemons;
+    }
+}
